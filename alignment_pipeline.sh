5fc80f917bda8dcfc120adf8785f2a932316de14
parasitehunter <christian.parobek@gmail.com>
2014-09-05 12:56:58 -0400

    removed BQSR from alignment_pipeline

--- alignment_pipeline.sh
+++ alignment_pipeline.sh
@@ -15,10 +15,16 @@
 for name in `cat filenames_test.txt`
 do
 
-## ALIGN PAIRED-END LANE #1 AND LANE #2 READS TO REF SEQ
-#bowtie2 --threads 8 -x /proj/julianog/refs/PvSAL1_v10.0/PvSal1_10.0 -1 /proj/julianog/sequence_reads/beckman_seq_backups/2014_07_22_AV_WGS_Libraries/Fastq/$name\_R1-lane1.fastq -2 /proj/julianog/sequence_reads/beckman_seq_backups/2014_07_22_AV_WGS_Libraries/Fastq/$name\_R2-lane1.fastq -S alignments/$name-lane1.sam --rg-id $name-lane1 --rg PL:illumina --rg LB:$name --rg PU:1 --rg SM:$name
+## ALIGN PAIRED-END READS WITH BWA ---- SUPPOSED TO USE bwa mem INSTEAD OF bwa aln/sampe
+bwa mem -M -t 8 /proj/julianog/refs/PvSAL1_v10.0/PlasmoDB-10.0_PvivaxSal1_Genome.fasta /proj/julianog/sequence_reads/beckman_seq_backups/2014_07_22_AV_WGS_Libraries/Fastq/$name\_R1-lane1.fastq /proj/julianog/sequence_reads/beckman_seq_backups/2014_07_22_AV_WGS_Libraries/Fastq/$name\_R2-lane1.fastq > alignments/$name-bwa_mem-lane1.sam
+	# -M marks shorter split hits as secondary (for Picard compatibility)
+	# -t indicates number of threads
+	# Is there a flag that suppresses all the massive output STDOUT of bwa-mem?
 
-#bowtie2 --threads 8 -x /proj/julianog/refs/PvSAL1_v10.0/PvSal1_10.0 -1 /proj/julianog/sequence_reads/beckman_seq_backups/2014_07_22_AV_WGS_Libraries/Fastq/$name\_R1-lane2.fastq -2 /proj/julianog/sequence_reads/beckman_seq_backups/2014_07_22_AV_WGS_Libraries/Fastq/$name\_R2-lane2.fastq -S alignments/$name-lane2.sam --rg-id $name-lane2 --rg PL:illumina --rg LB:$name --rg PU:1 --rg SM:$name
+## ALIGN PAIRED-END LANE #1 AND LANE #2 READS TO REF SEQ
+#bowtie2 --threads 8 -x /proj/julianog/refs/PvSAL1_v10.0/PvSal1_10.0 -1 /proj/julianog/sequence_reads/beckman_seq_backups/2014_07_22_AV_WGS_Libraries/Fastq/$name\_R1-lane1.fastq -2 /proj/julianog/sequence_reads/beckman_seq_backups/2014_07_22_AV_WGS_Libraries/Fastq/$name\_R2-lane1.fastq -S alignments/$name-lane1.sam --rg-id $name-lane1 --rg PL:illumina --rg LB:$name --rg SM:$name
+
+#bowtie2 --threads 8 -x /proj/julianog/refs/PvSAL1_v10.0/PvSal1_10.0 -1 /proj/julianog/sequence_reads/beckman_seq_backups/2014_07_22_AV_WGS_Libraries/Fastq/$name\_R1-lane2.fastq -2 /proj/julianog/sequence_reads/beckman_seq_backups/2014_07_22_AV_WGS_Libraries/Fastq/$name\_R2-lane2.fastq -S alignments/$name-lane2.sam --rg-id $name-lane2 --rg PL:illumina --rg LB:$name --rg SM:$name
 	# Awesome post about @RG entries http://seqanswers.com/forums/showthread.php?t=9784
 
 ## MERGE, SORT, AND COMPRESS SAM FILES
@@ -35,23 +41,22 @@ do
 #java -jar /nas02/apps/biojars-1.0/GenomeAnalysisTK-3.2-2/GenomeAnalysisTK.jar -T RealignerTargetCreator -R /proj/julianog/refs/PvSAL1_v10.0/PlasmoDB-10.0_PvivaxSal1_Genome.fasta -I alignments/$name.dedup.bam -o alignments/$name.realigner.intervals -nt 8
 
 ## PERFORM THE ACTUAL REALIGNMENT
-java -jar /nas02/apps/biojars-1.0/GenomeAnalysisTK-3.2-2/GenomeAnalysisTK.jar -T IndelRealigner -R /proj/julianog/refs/PvSAL1_v10.0/PlasmoDB-10.0_PvivaxSal1_Genome.fasta -I alignments/$name.dedup.bam -targetIntervals alignments/$name.realigner.intervals -o alignments/$name.realn.bam
-
-## INDEX BAM FILE FOR RECALIBRATION
-#java -jar /nas02/apps/picard-1.88/picard-tools-1.88/BuildBamIndex.jar INPUT=alignments/$name.merged.bam
-
-## MODEL THE ERROR MODES AND RECALIBRATE BASE QUALITIES
-#java -jar /nas02/apps/biojars-1.0/GenomeAnalysisTK-3.2-2/GenomeAnalysisTK.jar -T BaseRecalibrator -R /proj/julianog/refs/PvSAL1_v10.0/PlasmoDB-10.0_PvivaxSal1_Genome.fasta -I alignments/$name.dedup.bam -knownSites aln_bqsr/plasmoDB_vivax_snps.vcf -o aln_bqsr/$name.recalc 
-#-plots aln_bqsr/$name.recalc.pdf
+#java -jar /nas02/apps/biojars-1.0/GenomeAnalysisTK-3.2-2/GenomeAnalysisTK.jar -T IndelRealigner -R /proj/julianog/refs/PvSAL1_v10.0/PlasmoDB-10.0_PvivaxSal1_Genome.fasta -I alignments/$name.dedup.bam -targetIntervals alignments/$name.realigner.intervals -o alignments/$name.realn.bam
 
 ## VARIANT-CALLING USING UNIFIED GENOTYPER (GATK'S CALLER OF CHOICE FOR NON-DIPLOID)
 #java -jar /nas02/apps/biojars-1.0/GenomeAnalysisTK-3.2-2/GenomeAnalysisTK.jar -T UnifiedGenotyper -R /proj/julianog/refs/PvSAL1_v10.0/PlasmoDB-10.0_PvivaxSal1_Genome.fasta -I alignments/$name.realn.bam -o alignments/$name.vcf -ploidy 1 -nt 8
 
 
-# CALCULATE COVERAGE
+done
+
+######## EXTRA TOOLS #########
+
+## CALCULATE COVERAGE
 #bedtools genomecov -ibam alignments/$name.sorted.bam -max 10 | grep genome > $name.cov
 
-done
+## COUNT READS
+#java -jar /nas02/apps/biojars-1.0/GenomeAnalysisTK-3.2-2/GenomeAnalysisTK.jar -T CountReads -R /proj/julianog/refs/PvSAL1_v10.0/PlasmoDB-10.0_PvivaxSal1_Genome.fasta -I alignments/$name.merged.bam -rf MappingQualityZero
+
 
 
 
@@ -88,4 +93,6 @@ done
 
 ## To validate VCF format
 #java -Xmx2g -jar /nas02/apps/biojars-1.0/GenomeAnalysisTK-3.2-2/GenomeAnalysisTK.jar -R /proj/julianog/refs/PvSAL1_v10.0/PlasmoDB-10.0_PvivaxSal1_Genome.fasta -T ValidateVariants --validationTypeToExclude ALL --variant plasmoDB_vivax_snps.vcf
+
+
 
